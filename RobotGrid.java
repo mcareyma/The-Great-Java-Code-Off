250317ad0990e524990d4a789240fb34761378e7
JackDevN <94063348+JackDevN@users.noreply.github.com>
2023-07-07 12:28:44 +0100

    Update RobotGrid.java

--- RobotGrid.java
+++ RobotGrid.java
@@ -1,18 +1,44 @@
 public class RobotGrid {
-    // Placeholder code for the main implementation class
 
-    public boolean isSafe(int x, int y) {
-        // TODO: Add implementation logic here
-        throw new UnsupportedOperationException("Not implemented yet");
+    public static int calculateDigitSum(int num)
+    {
+        num = Math.abs(num);
+        int sum = 0;
+
+        while (num != 0)
+        {
+            sum = sum + num % 10;
+            num = num/10;
+        }
+        return sum;
     }
 
-    public int totalSafeSquares() {
-        // TODO: Add implementation logic here
-        throw new UnsupportedOperationException("Not implemented yet");
+    // Determines whether a given coordinate (x, y) is safe to visit
+    public static boolean isSafe(int x, int y) {
+        int sum = calculateDigitSum(x) + calculateDigitSum(y);
+        return sum <= 23;
     }
 
-    public int shortestSafeJourney(int a, int b, int x, int y) {
-        // TODO: Add implementation logic here
-        throw new UnsupportedOperationException("Not implemented yet");
+    // Calculates the total number of safe squares in the 2D grid
+    public static int totalSafeSquares() {
+        int count = 0;
+
+        for (int x = 0; x <= 100; x++) {
+            for (int y = 0; y <= 100; y++) {
+                if (isSafe(x, y)) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+
+        System.out.println(totalSafeSquares());
+
+
     }
 }
