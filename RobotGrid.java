da31fdfcdec420b37db8ed053fd931e0b827d6f1
Matthew Carey <matthew.carey@htmcts.net>
2023-07-07 12:54:58 +0100

    works2

--- RobotGrid.java
+++ RobotGrid.java
@@ -27,14 +27,14 @@ public class RobotGrid {
     // Calculates the total number of safe squares in the 2D grid that are reachable by the robot
     public static int totalSafeSquares() {
         int count = 0;
-        boolean[][] visited = new boolean[101][101]; // Keep track of visited squares
+        boolean[][] visited = new boolean[1400][1400]; // Keep track of visited squares
         Queue<int[]> queue = new LinkedList<>();
         int[] dx = {0, 1, 0, -1}; // Possible movements: right, down, left, up
         int[] dy = {1, 0, -1, 0};
 
         // Start BFS from the initial position (0, 0)
         queue.offer(new int[]{0, 0});
-        visited[0][0] = true;
+        visited[700][700] = true;
 
         while (!queue.isEmpty()) {
             int[] curr = queue.poll();
@@ -51,9 +51,9 @@ public class RobotGrid {
                     int ny = y + dy[i];
 
                     // Check if the neighboring square is within the grid bounds and hasn't been visited
-                    if (nx >= 0 && nx <= 100 && ny >= 0 && ny <= 100 && !visited[nx][ny]) {
+                    if (nx >= -700 && nx <= 700 && ny >= -700 && ny <= 700 && !visited[nx+700][ny+700]) {
                         queue.offer(new int[]{nx, ny});
-                        visited[nx][ny] = true;
+                        visited[nx+700][ny+700] = true;
                     }
                 }
             }
